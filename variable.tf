1321435daa4ba58a8ba9648b006221e9853cd10c
manzurshaikh <34501792+manzurshaikh@users.noreply.github.com>
2021-05-23 14:08:35 +0530

    define variable config

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "subnet" {
+    default = "bssubnet"
+}
+
+variable "env" {
+    default = "dev"
+}
+
+variable "region" {
+    default = "southeastasia" 
+}
+
+variable "virtual_network_name" {
+    default = "dev-bsvnet"
+}
+
+variable "azurerm_resource_group" {
+   default = "dev-bsrg"
+}
