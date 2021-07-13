b0cc32a4f9a6429e00c704f38905d73bd4cd3d42
raiz-manjur <manjur@raiz.com.my>
2021-07-13 10:46:29 +0530

    update app_insights

--- modules/function-basic/main.tf
+++ modules/function-basic/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_application_insights" "main" {
   name                = var.function_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  application_type    = "web"
+  application_type    = "Node.JS"
 }
 
 resource "azurerm_app_service_plan" "main" {
