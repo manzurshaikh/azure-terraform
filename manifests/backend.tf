752585eead0afa91c85ad30f717bbc5afb6e49fa
raiz-manjur <manjur@raiz.com.my>
2021-07-15 14:54:04 +0530

    fix subnet

--- manifests/backend.tf
+++ manifests/backend.tf
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "backend" {
   address_prefixes     = ["10.0.0.0/24"]
   enforce_private_link_service_network_policies = true
   service_endpoints = ["Microsoft.Storage", "Microsoft.AzureCosmosDB", "Microsoft.ServiceBus", "Microsoft.Web", "Microsoft.ContainerRegistry"]
-  service_endpoint_policy_ids = toset(null)    #Enable from the console currently not supported
+  service_endpoint_policy_ids = toset(null)    #Enable from the console currently not supported in Terraform
 
   delegation {
     name = "delegation"
@@ -79,101 +79,101 @@ module "storage_bsai" {
   account_replication_type  = var.account_replication_type_bsai
 }
 
-#module "container_registery" {
-#  depends_on               = [module.resource_group]
-#  source                   = "./../modules/contreg"
-#  contreg_name             = "${var.env}bsai"
-#  resource_group_name      = "${var.env}-bsai"
-#  contreg_location         = "${var.region}"
-#  
-#}
-#
-#module "app_service1" {
-#  depends_on                      = [module.resource_group]
-#  source                          = "./../modules/appservice"
-#  app_name                        = "${var.env}-${var.app_service1}"
-#  location                        = "${var.region}"
-#  resource_group_name             = "${var.env}-bsai"
-#  app_service_name                = "${var.env}-${var.app_service1}"
-#  virtual_network_name            = azurerm_subnet.backend.id
-#  docker_registry_server_url      = var.docker_registry_server_url
-#  docker_registry_server_username = var.docker_registry_server_username
-#  docker_registry_server_password = var.docker_registry_server_password
-#  docker_custom_image_name        = var.docker_custom_image_name
-#  linux_fx_version                = var.linux_fx_version
-#}
-#
-##module "app_service2" {
-##  depends_on           = [module.resource_group]
-##  source               = "./../modules/appservice"
-##  app_name             = "${var.env}-${var.app_service2}"
-##  location             = "${var.region}"
-##  resource_group_name  = "${var.env}-bsai"
-##  app_service_name     = "${var.env}-${var.app_service2}"
-##  virtual_network_name = azurerm_subnet.backend.id
-##}
-#
-#module "azure_function1" {
-#  depends_on                      = [module.resource_group]
-#  source                          = "./../modules/function-basic"
-#  function_name                   = "${var.env}${var.function_name1}"
-#  location                        = "${var.region}"
-#  resource_group_name             = "${var.env}-bsai"
-#  storageaccount_name             = "${var.function_name1}"
-#  virtual_network_name            = azurerm_subnet.backend.id
-#}
-#
-#module "azure_function2" {
+module "container_registery" {
+  depends_on               = [module.resource_group]
+  source                   = "./../modules/contreg"
+  contreg_name             = "${var.env}bsai"
+  resource_group_name      = "${var.env}-bsai"
+  contreg_location         = "${var.region}"
+  
+}
+
+module "app_service1" {
+  depends_on                      = [module.resource_group]
+  source                          = "./../modules/appservice"
+  app_name                        = "${var.env}-${var.app_service1}"
+  location                        = "${var.region}"
+  resource_group_name             = "${var.env}-bsai"
+  app_service_name                = "${var.env}-${var.app_service1}"
+  virtual_network_name            = azurerm_subnet.backend.id
+  docker_registry_server_url      = var.docker_registry_server_url
+  docker_registry_server_username = var.docker_registry_server_username
+  docker_registry_server_password = var.docker_registry_server_password
+  docker_custom_image_name        = var.docker_custom_image_name
+  linux_fx_version                = var.linux_fx_version
+}
+
+#module "app_service2" {
 #  depends_on           = [module.resource_group]
-#  source               = "./../modules/function-basic"
-#  function_name        = "${var.env}${var.function_name2}"
+#  source               = "./../modules/appservice"
+#  app_name             = "${var.env}-${var.app_service2}"
 #  location             = "${var.region}"
 #  resource_group_name  = "${var.env}-bsai"
-#  storageaccount_name  = "${var.function_name1}"
+#  app_service_name     = "${var.env}-${var.app_service2}"
 #  virtual_network_name = azurerm_subnet.backend.id
 #}
-#
-#module "azure_function3" {
-#  depends_on           = [module.resource_group]
-#  source               = "./../modules/function-basic"
-#  function_name        = "${var.env}${var.function_name3}"
-#  location             = "${var.region}"
-#  resource_group_name  = "${var.env}-bsai"
-#  storageaccount_name  = "${var.function_name1}"
-#  virtual_network_name = azurerm_subnet.backend.id
-#}
-#
-#module "cosmosdb_1" {
-#  depends_on                   = [module.vnet]
-#  source                       = "./../modules/cosmosdb"
-#  cosmodb_account_name         = "${var.env}${var.cosmodb_account_name_1}"
-#  resource_group_name          = "${var.env}-bsai"
-#  ip_range_filter              = var.ip_range_filter
-#  location                     = "${var.region}"
-#  cosmosdb_name                = var.cosmosdb_name_1
-#  enable_automatic_failover    = var.enable_automatic_failover
-#  failover_location_secondary  = var.failover_location_secondary
-#  failover_priority_secondary  = var.failover_priority_secondary
-#  vnet_subnet_id               = azurerm_subnet.backend.id    
-#}
-#
-#module "servicebus_1" {
-#  depends_on            = [module.resource_group]
-#  source                = "./../modules/service-bus"
-#  servicebus_name       = "${var.env}${var.servicebus_name_1}"
-#  resource_group_name   = "${var.env}-bsai"
-#  location              = "${var.region}"
-#  servicebus_queue_name = var.servicebus_queue_name_1
-#}
-#
-#module "servicebus_2" {
-#  depends_on            = [module.resource_group]
-#  source                = "./../modules/service-bus"
-#  servicebus_name       = "${var.env}${var.servicebus_name_2}"
-#  resource_group_name   = "${var.env}-bsai"
-#  location              = "${var.region}"
-#  servicebus_queue_name = var.servicebus_queue_name_2
-#}
+
+module "azure_function1" {
+  depends_on                      = [module.resource_group]
+  source                          = "./../modules/function-basic"
+  function_name                   = "${var.env}${var.function_name1}"
+  location                        = "${var.region}"
+  resource_group_name             = "${var.env}-bsai"
+  storageaccount_name             = "${var.function_name1}"
+  virtual_network_name            = azurerm_subnet.backend.id
+}
+
+module "azure_function2" {
+  depends_on           = [module.resource_group]
+  source               = "./../modules/function-basic"
+  function_name        = "${var.env}${var.function_name2}"
+  location             = "${var.region}"
+  resource_group_name  = "${var.env}-bsai"
+  storageaccount_name  = "${var.function_name1}"
+  virtual_network_name = azurerm_subnet.backend.id
+}
+
+module "azure_function3" {
+  depends_on           = [module.resource_group]
+  source               = "./../modules/function-basic"
+  function_name        = "${var.env}${var.function_name3}"
+  location             = "${var.region}"
+  resource_group_name  = "${var.env}-bsai"
+  storageaccount_name  = "${var.function_name1}"
+  virtual_network_name = azurerm_subnet.backend.id
+}
+
+module "cosmosdb_1" {
+  depends_on                   = [module.vnet]
+  source                       = "./../modules/cosmosdb"
+  cosmodb_account_name         = "${var.env}${var.cosmodb_account_name_1}"
+  resource_group_name          = "${var.env}-bsai"
+  ip_range_filter              = var.ip_range_filter
+  location                     = "${var.region}"
+  cosmosdb_name                = var.cosmosdb_name_1
+  enable_automatic_failover    = var.enable_automatic_failover
+  failover_location_secondary  = var.failover_location_secondary
+  failover_priority_secondary  = var.failover_priority_secondary
+  vnet_subnet_id               = azurerm_subnet.backend.id    
+}
+
+module "servicebus_1" {
+  depends_on            = [module.resource_group]
+  source                = "./../modules/service-bus"
+  servicebus_name       = "${var.env}${var.servicebus_name_1}"
+  resource_group_name   = "${var.env}-bsai"
+  location              = "${var.region}"
+  servicebus_queue_name = var.servicebus_queue_name_1
+}
+
+module "servicebus_2" {
+  depends_on            = [module.resource_group]
+  source                = "./../modules/service-bus"
+  servicebus_name       = "${var.env}${var.servicebus_name_2}"
+  resource_group_name   = "${var.env}-bsai"
+  location              = "${var.region}"
+  servicebus_queue_name = var.servicebus_queue_name_2
+}
 
 /* OUTOUT */
 output "subnet_backend_id" {
--- manifests/variable.tf
+++ manifests/variable.tf
@@ -31,68 +31,68 @@ variable "account_replication_type_bsai" {
 }
 
 
-#/* Azure Function */
-#variable "function_name1" {
+/* Azure Function */
+variable "function_name1" {
+}
+
+variable "function_name2" {
+}
+
+variable "function_name3" {
+}
+
+/* DATABASE */
+variable "cosmodb_account_name_1" {
+}
+
+variable "enable_automatic_failover" {
+}
+
+variable "failover_location_secondary" {
+}
+
+variable "failover_priority_secondary" {
+}
+
+variable "cosmosdb_name_1" {
+}
+
+variable "ip_range_filter" {
+    type        = string
+    default = "0.0.0.0"
+}
+
+/*SERVICE_BUS */
+variable "servicebus_name_1" {
+}
+
+variable "servicebus_queue_name_1" {
+}
+
+variable "servicebus_name_2" {
+}
+
+variable "servicebus_queue_name_2" {
+}
+
+/* APP_SERVICE */
+variable "app_service1" { 
+}
+
+#variable "app_service2" { 
 #}
-#
-#variable "function_name2" {
-#}
-#
-#variable "function_name3" {
-#}
-#
-#/* DATABASE */
-#variable "cosmodb_account_name_1" {
-#}
-#
-#variable "enable_automatic_failover" {
-#}
-#
-#variable "failover_location_secondary" {
-#}
-#
-#variable "failover_priority_secondary" {
-#}
-#
-#variable "cosmosdb_name_1" {
-#}
-#
-#variable "ip_range_filter" {
-#    type        = string
-#    default = "0.0.0.0"
-#}
-#
-#/*SERVICE_BUS */
-#variable "servicebus_name_1" {
-#}
-#
-#variable "servicebus_queue_name_1" {
-#}
-#
-#variable "servicebus_name_2" {
-#}
-#
-#variable "servicebus_queue_name_2" {
-#}
-#
-#/* APP_SERVICE */
-#variable "app_service1" { 
-#}
-#
-##variable "app_service2" { 
-##}
-#
-#variable "docker_registry_server_url" {
-#}
-#
-#variable "docker_registry_server_username" {
-#}
-#
-#variable "docker_registry_server_password" {
-#}
-#
-#variable "docker_custom_image_name" {
-#}
-#
-#variable "linux_fx_version" {
-#}
+
+variable "docker_registry_server_url" {
+}
+
+variable "docker_registry_server_username" {
+}
+
+variable "docker_registry_server_password" {
+}
+
+variable "docker_custom_image_name" {
+}
+
+variable "linux_fx_version" {
+}
