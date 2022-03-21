690e3b0f76a3c475238fcecb22e762d4a89ce6a1
raiz-manjur <manjur@raiz.com.my>
2022-03-21 23:54:34 +0530

    remove aks

--- manifests/aks.tf
+++ manifests/aks.tf
@@ -16,21 +16,21 @@ resource "azurerm_subnet" "akssubnet" {
 #  service_endpoints    = ["Microsoft.Storage", "Microsoft.AzureCosmosDB", "Microsoft.ServiceBus", "Microsoft.Web", "Microsoft.ContainerRegistry"]
 #}
 
-module "aks_bsai" {
-  source               = "./../modules/aks"
-  aks_name             = "${var.env}-aks"
-  aks_dns_name         = "${var.env}-aks"
-  location             = "${var.region}"
-  resource_group_name  = "${var.env}-bsai"
-  virtual_network_name = "${var.env}-${var.region}-bsai"
-  aks_subnet_id        = azurerm_subnet.akssubnet.id
-  appId                = var.appId
-  client_secret        = var.client_secret
-  private_cluster_enabled = false
-  private_cluster_public_fqdn_enabled = false
-}
-
-output "kube_config" {
-  value = module.aks_bsai.kube_config
-  sensitive = true
-}
+#module "aks_bsai" {
+#  source               = "./../modules/aks"
+#  aks_name             = "${var.env}-aks"
+#  aks_dns_name         = "${var.env}-aks"
+#  location             = "${var.region}"
+#  resource_group_name  = "${var.env}-bsai"
+#  virtual_network_name = "${var.env}-${var.region}-bsai"
+#  aks_subnet_id        = azurerm_subnet.akssubnet.id
+#  appId                = var.appId
+#  client_secret        = var.client_secret
+#  private_cluster_enabled = false
+#  private_cluster_public_fqdn_enabled = false
+#}
+#
+#output "kube_config" {
+#  value = module.aks_bsai.kube_config
+#  sensitive = true
+#}
